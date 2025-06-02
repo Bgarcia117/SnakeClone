1de75df04c87f84ad45fdaabd6a01888174aa454
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-06-01 21:33:12 -0500

    Add: grow function for increasing langth, move function to move, rendersnake to render onto screen, and enum for directions

--- include/snake.h
+++ include/snake.h
@@ -2,20 +2,36 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 #include <vector>
 
 class Snake {
-private:
-	// Snake Body
-	std::vector<sf::RectangleShape> body;
-
-
 public:
+	// === Enum for the snakes directions
+	enum class Direction {
+		Up,
+		Down, 
+		Left,
+		Right
+	};
+
 	// === Constructors / Destructor
 	Snake();
 	~Snake();
 
 	void grow();
-	void updatePositon();
+	void move(sf::Vector2u windowSize);
 	void renderSnake(sf::RenderTarget& target);
+	 
+private:
+	// === Constants ===
+	const float segmentSize = 20.f;
+
+	// === Variables ===
+	Direction moveDirection;
+
+	// === Snake Body ===
+	std::vector<sf::RectangleShape> body;
+
+
 };
--- src/snake.cpp
+++ src/snake.cpp
@@ -1,6 +1,7 @@
 #include "snake.h"
 
 Snake::Snake() {
+	moveDirection = Direction::Right;
 	grow();
 }
 
@@ -9,15 +10,39 @@ Snake::~Snake() {
 
 void Snake::grow() {
 	sf::RectangleShape bodySegment;
-	bodySegment.setSize({ 20.f, 20.f });
+	bodySegment.setSize({ segmentSize, segmentSize }); 
+	bodySegment.setFillColor(sf::Color::Green);
+
+	if (body.empty()) {
+		bodySegment.setPosition({ 200.f, 200.f });
+	}
+
 	body.push_back(bodySegment);
 }
 
-void Snake::updatePositon() {
+void Snake::move(sf::Vector2u windowSize) {
+	switch (moveDirection) {
+	case Direction::Up:
+		break;
+
+	case Direction::Down:
+		break;
+
+	case Direction::Left:
+		break;
+
+	case Direction::Right:
+		if (body[0].getPosition().x + segmentSize < windowSize.x)
+		    body[0].move({ segmentSize, 0.f });
+
+		break;
+	}
 
 }
 
 
 void Snake::renderSnake(sf::RenderTarget& target) {
-	target.draw(body[0]);
+	for (const auto& segment : body) {
+		target.draw(segment);
+	}
 }
