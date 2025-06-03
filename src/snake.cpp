4a48a7993194b231332dabe703a7839949cb4e96
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-06-02 21:39:42 -0500

    Improved: Move function to take directions from game class

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,7 +1,16 @@
 #include "snake.h"
 
 Snake::Snake() {
+	turnPos = startPos;
 	moveDirection = Direction::Right;
+
+	SnakeSegment head;
+	head.part.setSize({ segmentSize, segmentSize });
+	head.part.setFillColor(sf::Color::Cyan);
+	head.part.setPosition(startPos);
+
+
+	grow();
 	grow();
 }
 
@@ -14,35 +23,62 @@ void Snake::grow() {
 	bodySegment.setFillColor(sf::Color::Green);
 
 	if (body.empty()) {
-		bodySegment.setPosition({ 200.f, 200.f });
+		bodySegment.setPosition(startPos);
+	}
+	else {
+		bodySegment.setPosition({ tailPos.x - segmentSize, tailPos.y });
+		tailPos = bodySegment.getPosition();
 	}
 
 	body.push_back(bodySegment);
 }
 
-void Snake::move(sf::Vector2u windowSize) {
+void Snake::move(Direction direction) {
+	setDirection(direction);
+
 	switch (moveDirection) {
 	case Direction::Up:
+		for (auto& segment : body) {
+			if (segment.getPosition() == turnPos)
+			segment.move({ 0.f, -segmentSize });
+		}
 		break;
 
 	case Direction::Down:
+		for (auto& segment : body) {
+			segment.move({ 0.f, segmentSize });
+	    }
 		break;
 
 	case Direction::Left:
+		for (auto& segment : body) {
+			segment.move({ -segmentSize, 0.f });
+		}
 		break;
 
 	case Direction::Right:
-		if (body[0].getPosition().x + segmentSize < windowSize.x)
-		    body[0].move({ segmentSize, 0.f });
-
-		break;
+		for (auto& segment : body) {
+			segment.move({ segmentSize, 0.f });
+		}
 	}
 
 }
 
-
 void Snake::renderSnake(sf::RenderTarget& target) {
 	for (const auto& segment : body) {
 		target.draw(segment);
+	}
+}
+
+SnakeSegment Snake::createSegment(bool isHead, bool isTail) {
+	SnakeSegment segment;
+	segment.part.setSize({ segmentSize, segmentSize });
+	segment.part.setFillColor(isHead ? sf::Color::Cyan : sf::Color::Green);
+
+	if (body.empty()) {
+		segment.part.setPosition(startPos);
+	}
+	else {
+
 	}
 }
